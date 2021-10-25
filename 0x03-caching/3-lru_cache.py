37dcd4c6dd19f918148221d0f08ebb02fc93765d
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-10-25 15:59:03 -0500

    Add files via upload

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-"""Create a class LRUCache that inherits
-from BaseCaching and is a caching system
+"""LRUCache module
 """
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """least recently used caching system"""
-
-    USED = {}
-
+    """LRUCache class
+        BaseCaching (class): Basic class for this class
+    """
     def __init__(self):
         super().__init__()
+        self.__keys = []
 
     def put(self, key, item):
-        """Add chaching to caching system"""
-        if key is None or item is None:
-            pass
-        else:
-            self.cache_data.update({key: item})
-            self.USED.update({key: 0})
-            if self.cache_data.__len__() > super().MAX_ITEMS:
-                pop_item = min(self.USED)
-                if self.cache_data.get(pop_item) is not None:
-                    self.USED.pop(pop_item)
-                    self.cache_data.pop(pop_item)
-                print("DISCARD: {}".format(pop_item))
+        """put item into cache_data with LIFO algorithm
+        """
+        if len(self.cache_data) == self.MAX_ITEMS and key not in self.__keys:
+            discard = self.__keys.pop(0)
+            del self.cache_data[discard]
+            print('DISCARD: {}'.format(discard))
+        if key and item:
+            if key in self.cache_data:
+                self.__keys.remove(key)
+            self.__keys.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """Get cache from caching system"""
-        if key is None or self.cache_data.get(key) is None:
+        """get value of cache_data dictionary
+        """
+        if not key or key not in self.cache_data:
             return None
-
-        if self.USED.get(key) is not None:
-            self.USED[key] += 1
-        else:
-            self.USED[key] = 1
-
-        return self.cache_data.get(key)
+        self.__keys.remove(key)
+        self.__keys.append(key)
+        return self.cache_data[key]
