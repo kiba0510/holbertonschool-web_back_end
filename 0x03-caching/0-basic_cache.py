eccb842380a52ed5d0a07ebdf1f85ce14badb5da
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-10-25 15:44:27 -0500

    Add files via upload

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -2,7 +2,7 @@
 """Create a class BasicCache that inherits from
 BaseCaching and is a caching system
 """
-from BaseCaching import BaseCaching
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
