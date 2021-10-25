9019124713f3290b12e7aaed99b3f3cd5b04565f
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-10-25 15:24:28 -0500

    Add files via upload

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Create a class BasicCache that inherits from
+BaseCaching and is a caching system
+"""
+from BaseCaching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basics of caching"""
+
+    def put(self, key, item):
+        """Add chaching to caching system"""
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data.update({key: item})
+
+    def get(self, key):
+        """Get cache from caching system"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
