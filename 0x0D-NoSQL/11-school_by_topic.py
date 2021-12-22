168ba68d864fe2f7bd55bea713f57d6049747dd3
Julian Archbold <70171411+kiba0510@users.noreply.github.com>
2021-12-22 10:36:57 -0500

    Add files via upload

---
+++ 0x0D-NoSQL/11-school_by_topic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" returns the list of school having a specific topic """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ return school by topic
+    """
+    schools = mongo_collection.find({
+        "topics": {
+            "$in": [topic]
+        }
+    })
+    return schools
+    
